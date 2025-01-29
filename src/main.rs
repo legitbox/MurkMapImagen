3dd1a0849821767f6b117ee7fb1dc63bafc09d22
legitbox <materixasqwerty@gmail.com>
2025-01-29 13:18:36 +0200

    first version of the finished renderer

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,70 @@
 use bincode;
 use flate2::read::GzDecoder;
-use image::{Rgba, RgbaImage};
+use image::{Rgba, RgbaImage, imageops, GenericImage, Pixel};
 use std::fs::File;
 use std::io::{self, Read};
 
-fn create_chunk_image(chunk_scan: Vec<(i32,i32,i32,String)>) {
-    let x_size:u8 = 16;
-    let z_size:u8 = 16;
-    let layer_count:u16 = 320;
+fn is_all_zeros(image: &RgbaImage) -> bool {
+    !image.pixels().any(|pixel| pixel.channels() != [0, 0, 0, 0])
+}
 
+fn create_chunk_image(chunk_scan: Vec<(i32,i32,i32,String)>, chunk_coordinates: (i32,i32)) {
+    let normalised_coordinates:(u32,u32) = ((chunk_coordinates.0 * 16) as u32, (chunk_coordinates.1*16) as u32);
+    let mut chunk_image_vector: Vec<RgbaImage> = Vec::new();
+    let mut final_chunk_image = RgbaImage::new(16, 16);
+    for y in -64..320 {
+        let mut image = RgbaImage::new(16,16);
+        for block in chunk_scan.iter() {
+            if block.1 == y {
+                if block.3 == "minecraft:grass_block"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([112, 137, 57, 255]));
+                }
+                if block.3 == "minecraft:water"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([70, 148, 168, 120]));
+                }
+                if block.3 == "minecraft:oak_leaves"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([78, 92, 44, 200]));
+                }
+                if block.3 == "minecraft:oak_log"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([148, 72, 64, 255]));
+                }
+                if block.3 == "minecraft:dirt"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([182, 106, 77, 255]));
+                }
+                if block.3 == "minecraft:stone"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([182, 106, 77, 255]));
+                }
+                if block.3 == "minecraft:gravel"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([220, 222, 209, 255]));
+                }
+                if block.3 == "minecraft:clay"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([165, 176, 182, 255]));
+                }
+                if block.3 == "minecraft:short_grass"{
+                    image.put_pixel(block.0 as u32-normalised_coordinates.0, block.2 as u32-normalised_coordinates.1, Rgba([78, 92, 44, 100]));
+                }
+            }
+        }
+        chunk_image_vector.push(image);
+    }
+    let mut brightness_index = 0;
+    for layer in &mut chunk_image_vector {
+        if is_all_zeros(&layer){
+            continue;
+        }
+        imageops::overlay(&mut final_chunk_image, layer, 0, 0);
+        brightness_index += 1;
+    }
+    final_chunk_image.save("./imgs/result.png".to_string()).unwrap();
 }
 
 fn main() -> io::Result<()> {
-    // Open the compressed binary file
     let file = File::open("./chunk_binary_data/0_0.mmbf")?;
-
-    // Create a GzDecoder to decompress the data
     let mut decoder = GzDecoder::new(file);
-
-    // Read the decompressed data into a Vec<u8>
     let mut decompressed_data = Vec::new();
     decoder.read_to_end(&mut decompressed_data)?;
-
-    // Deserialize the decompressed data back into the original structure
     let chunk_scan: Vec<(i32, i32, i32, String)> = bincode::deserialize(&decompressed_data).unwrap();
 
-    create_chunk_image(chunk_scan);
+    create_chunk_image(chunk_scan, (0,0));
     Ok(())
 }
